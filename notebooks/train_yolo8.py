6c90037a7dc4f1813666943adcdcd45362628411
Abdullin Ilgiz <abdyllin-ilgiz@mail.ru>
2024-01-14 20:26:12 +0500

    Fixed small issues with routes, requirements, readme

--- notebooks/train_yolo8.py
+++ notebooks/train_yolo8.py
@@ -1,9 +1,16 @@
+import os
+from pathlib import Path
+
 from ultralytics import YOLO
+
+
+directory_name = Path(__file__).resolve().parent.parent
+dataset_yaml_path = os.path.join(directory_name, 'notebooks', 'datasetv8.yaml')
 
 model = YOLO('yolov8s.pt')
 
 results = model.train(
-    data='notebooks/datasetv8.yaml',
+    data=dataset_yaml_path,
     epochs=100,
     imgsz=960,
     batch=4
